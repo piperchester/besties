ad918c8467af0000661cf0d08694be598d45a009
Nate Perry <ndp4570@g.rit.edu>
2014-04-10 23:01:40 -0400

    Cleaned up markup, Various JS adjustments

--- js/besties.js
+++ js/besties.js
@@ -1,4 +1,69 @@
-  $('#facepile').hide();
+
+  //cache jQuery selectors
+  var $doc = $(document);
+  // PROTIP: $.find() is faster than $()
+  var $facepile = $doc.find('#facepile'),
+      $authModal = $doc.find('#authModal'),
+      $fbFriendForm = $doc.find('#fbFriendForm'),
+      $scrapeButton = $doc.find('#fbScrapeButton'),
+      $pit = $doc.find('#pit');
+
+  //globals
+  var Besties, KeyHandler;
+
+  //main controller for app
+  Besties = {
+    init : function () {
+      console.log('Besties initiated');
+      $facepile.hide();
+      // PROTIP:
+      // Within this init function, `this` is a reference to `window`, not Besties
+      // Functions being called through a jQuery event will have jQuery has their reference of `this`
+      Besties.attachListeners();
+    },
+    //add all event listeners
+    attachListeners : function () {
+      $scrapeButton.on('click', this.scrapeFriends); // PROTIP: parenthesis is not needed
+      $doc.on('keypress', KeyHandler.onKeyPress);
+    },
+    // Takes friend ID string from input form and searches graph for Close Friends member names, appends to site
+    scrapeFriends : function () {
+      var friendID = $fbFriendForm.val().trim();
+      if (friendID === '') {
+        alert('Woahh, slow down there buddy. You have to enter a friend code first.');
+        return false;
+      }
+      FB.api(friendID + '/friendlists/close_friends?fields=members', function (response) {
+        if (!response || response.error) {
+          alert('Hmm - that was unexpected. Are you sure you have an access token?');
+          return false;
+        } else {
+          $pit.remove();
+          var friendCounter = 0;
+          var html = '';
+          for (friend in response.data[0].members.data){
+            html += '<section classs="alert alert-info">' + response.data[0].members.data[friendCounter++].name + '</section>';
+          }
+          $facepile.append(html);
+        }
+        $facepile.show();
+      });
+    }
+  };
+
+  //handles all key events
+  KeyHandler = {
+    //determines what to do based on which key is pressed
+    onKeyPress : function (key) {
+      var code = key.keyCode;
+      //using a switch to allow for easy addition of new cases later
+      switch (code) {
+        case 13:
+          Besties.scrapeFriends();
+          break;
+      }
+    }
+  }
 
   window.fbAsyncInit = function () {
     FB.init({
@@ -10,22 +75,22 @@
 
     FB.getLoginStatus(function (response) {
       if (response.status === 'connected') {
-        $('#authModal').modal('hide');
+        $authModal.modal('hide');
       } else if (response.status === 'not_authorized') {
         // the user is logged in to Facebook, but has not authenticated the app
-        $('#authModal').modal('show');
+        $authModal.modal('show');
       } else {
-        $('#authModal').modal('show');
+        $authModal.modal('show');
       }
     });
 
     FB.Event.subscribe('auth.authResponseChange', function (response) {
       if (response.status === 'connected') {
-        $('#authModal').modal('hide');
+        $authModal.modal('hide');
       } else if (response.status === 'not_authorized') {
-        $('#authModal').modal('show');
+        $authModal.modal('show');
       } else {
-        $('#authModal').modal('show');
+        $authModal.modal('show');
       }
     });
   };
@@ -39,20 +104,4 @@
     ref.parentNode.insertBefore(js, ref);
   }(document));
 
-  // Takes friend ID string from input form and searches graph for Close Friends member names, appends to site
-  function scrapeFriends() {
-    var friendID = $('#fbFriendForm').val();
-    FB.api(friendID + '/friendlists/close_friends?fields=members', function (response) {
-      if (!response || response.error) {
-        alert('Hmm - that was unexpected. Are you sure you have an access token?');
-      } else {
-        $('#pit').remove();
-        var friendCounter = 0;
-        for (friend in response.data[0].members.data){
-          $('#facepile').append('<section classs="alert alert-info">' + response.data[0].members.data[friendCounter++].name + '</section><br><br>');
-        }
-      }
-
-      $('#facepile').show();
-    });
-  }
+  window.onload = Besties.init;
